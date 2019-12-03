10cc945624bfeffbc2fc3fa1197ab0b9a7daf110
Nikita Orlyakov <nikita.orlyakov@gmail.com>
2019-12-03 23:14:31 +0300

    Fixes for models.

--- exchange/exchange/stocks/models.py
+++ exchange/exchange/stocks/models.py
@@ -5,13 +5,15 @@ from django.db import models
 
 
 class Currency(models.Model):
-    value = models.TextField(max_length=100)
+    value = models.CharField(max_length=100)
 
     def __str__(self):
         return f'{self.value}'
 
 
 class Stock(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     currency = models.ForeignKey(Currency, on_delete=models.PROTECT)
     value = models.PositiveIntegerField(default=0)
@@ -26,6 +28,7 @@ class Stock(models.Model):
 
 class TransactionTypes(enum.Enum):
     common = enum.auto()
+    exchange = enum.auto()
     commission = enum.auto()
     canceled = enum.auto()
     revoke = enum.auto()
@@ -34,16 +37,19 @@ class TransactionTypes(enum.Enum):
 class Transaction(models.Model):
     TRANSACTION_CHOICES = [
         ('CMN', TransactionTypes.common.name),
+        ('EXC', TransactionTypes.exchange.name),
         ('CMS', TransactionTypes.commission.name),
         ('CNL', TransactionTypes.canceled.name),
         ('RVK', TransactionTypes.revoke.name),
     ]
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
     stock_from = models.ForeignKey(Stock, on_delete=models.PROTECT, null=True, related_name='+')
     stock_to = models.ForeignKey(Stock, on_delete=models.PROTECT, null=True, related_name='+')
     value = models.IntegerField()
-    type = models.TextField(max_length=3, choices=TRANSACTION_CHOICES,
+    type = models.CharField(max_length=3, choices=TRANSACTION_CHOICES,
                             default=TransactionTypes.common.name)
-    related_transaction = models.ForeignKey('self', on_delete=models.CASCADE, null=True, default=None)
+    related_transaction = models.ForeignKey('self', on_delete=models.PROTECT, null=True, default=None)
 
     def __str__(self):
         return f'{self.value} {self.stock_from.currency}'
