7d595dc2af111419294927853b270c90702bc7e9
Nikita Orlyakov <nikita.orlyakov@gmail.com>
2019-12-03 21:33:24 +0300

    Added comment to settings.py.

--- exchange/exchange/settings.py
+++ exchange/exchange/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
-    'django.contrib.contenttypes',
+    'django.contrib.contenttypes',  # DO WE NEED THIS?
     'django.contrib.sessions',
     'django.contrib.messages',
 ]
