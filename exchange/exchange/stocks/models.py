fca2a3fa7052818525910f1da0b0cd332ea7eae5
Nikita Orlyakov <nikita.orlyakov@gmail.com>
2019-12-04 23:38:05 +0300

    Added created updated to currency model.

--- exchange/exchange/stocks/models.py
+++ exchange/exchange/stocks/models.py
@@ -8,6 +8,8 @@ from django.db import models
 
 class Currency(models.Model):
     value = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'{self.value}'
