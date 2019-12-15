92830455dd867d346026776862cad01c24d4afcd
Nikita Orlyakov <nikita.orlyakov@gmail.com>
2019-12-15 14:26:44 +0300

    Changed serializers.

--- exchange/exchange/stocks/serializers.py
+++ exchange/exchange/stocks/serializers.py
@@ -3,14 +3,15 @@
 :Authors: norlyakov
 :Date: 11.12.2019
 """
+from django.db import IntegrityError
 from rest_framework import serializers
 
-from . import models
+from .models import Currency, Stock
 
 
 class CurrencySerializer(serializers.ModelSerializer):
     class Meta:
-        model = models.Currency
+        model = Currency
         fields = ['id', 'name', 'code']
 
     def create(self, validated_data):
@@ -23,11 +24,23 @@ class CurrencySerializer(serializers.ModelSerializer):
 class StockSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     user = serializers.ReadOnlyField(source='user.username')
-    currency = serializers.PrimaryKeyRelatedField(queryset=models.Currency.objects.all())
+    currency = serializers.CharField(max_length=10, source='currency.code')
     value = serializers.DecimalField(max_digits=100, decimal_places=5, read_only=True)
 
     def create(self, validated_data):
-        return models.Stock.objects.create(**validated_data)
+        currency_code = validated_data.pop('currency')['code']
+        try:
+            currency = Currency.objects.get(code=currency_code)
+        except Currency.DoesNotExist:
+            message = 'Currency with such code does not exist'
+            raise serializers.ValidationError(message)
+
+        try:
+            return Stock.objects.create(currency=currency, **validated_data)
+        except IntegrityError as e:
+            if 'UNIQUE' in e.args[0]:
+                message = 'User already have stock with this currency'
+                raise serializers.ValidationError(message)
 
     def update(self, instance, validated_data):
         raise RuntimeError('Stock can not be updated')
